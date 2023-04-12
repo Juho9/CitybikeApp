f52f63ac02eaf7846c4960fb60c99f7f082f89f2
GunnarSven <94739044+GunnarSven@users.noreply.github.com>
2023-04-12 03:17:32 +0300

    Small changes to Marker appearances

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Callout, Marker } from 'react-native-maps';
 import { StyleSheet, View, Text, Keyboard, TextInput, Button, TouchableWithoutFeedback, Image } from 'react-native';
 import { BikeRentalStation } from '../types/RoutingApi';
 import { FetchBikes } from '../utils/FetchBikes';
 
 
+
+
 const MapComponent: React.FC = () => {
 
-
+    
     const [location, setLocation] = React.useState({
         lat: 60.200692,
         lng: 24.934302,
       })
 
+    const favourites: string[] = []
     const [stations, setStations] = React.useState<BikeRentalStation[]>([])
 
     React.useState(() => {
@@ -32,12 +35,17 @@ const MapComponent: React.FC = () => {
         }
     }
 
+    const addToFavourite = (stationId: string) => {
+      favourites.push(stationId)
+      console.log(favourites)
+    }
+
     return (
         <View style={styles.container}>
           <MapView style={styles.map} 
-          region={{latitude: location.lat, longitude: location.lng, latitudeDelta: 0.0322, longitudeDelta: 0.0221 }} 
+          region={{latitude: location.lat, longitude: location.lng, latitudeDelta: 0.0122, longitudeDelta: 0.0121 }} 
           >
-            {stations.map(m => (
+            {stations.map(m =>  (
                 <Marker
                     key={m.stationId}
                     title={m.name}
@@ -45,9 +53,19 @@ const MapComponent: React.FC = () => {
                         latitude: m.lat,
                         longitude: m.lon
                     }}
-                    description={'Bikes available: ' + m.bikesAvailable}
-                >
-                    <Image source={getImageForMarker(m.bikesAvailable)} style={{height: 35, width:35 }} />
+                    image={(getImageForMarker(m.bikesAvailable))}
+                > 
+                    
+                    <Callout style={{justifyContent: 'center', alignItems: 'center'}}>
+                      <View style={{}}>
+                        <Text numberOfLines={1} style={{fontSize: 28, }}>{m.name}</Text>
+                        <View style={{ alignItems: 'center' }}>
+                          <Text numberOfLines={1} style={{fontSize: 15, }}>Bikes available: {m.bikesAvailable}</Text>
+                          <Text numberOfLines={1} style={{fontSize: 15, }}>Total spaces: {m.capacity}</Text>
+                          <Button title='Favourite' onPress={() => addToFavourite(m.stationId)} />
+                        </View>
+                      </View>
+                    </Callout>
                 </Marker>
             ))}
           </MapView>
@@ -65,6 +83,13 @@ const MapComponent: React.FC = () => {
         height: '100%',
         width: '100%',
       },
+      marker: {
+        backgroundColor: '#E6E6FA'
+      },
+      callout: {
+        alignSelf: 'flex-start',
+
+      }
     });
 
 export default MapComponent;
